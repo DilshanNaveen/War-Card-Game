921d793067741ffb3c5e2bd822c55eb4c5e70f09
Dilshan Naveen <dilshan@vertex.lk>
2023-03-11 15:40:05 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Dilshan Naveen on 2023-03-11.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
